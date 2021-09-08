16fffa53ef44dcab33c1518c0a864e02e403872a
Adarsh Raj <adarshrajgr2016@gmail.com>
2021-09-08 23:47:48 +0530

    Add files via upload

--- project on c++1 .cc
+++ project on c++1 .cc
@@ -50,12 +50,27 @@ int main()
 	   	string bageltype[]={"nature bagel","cheese bagel","egg bagel"};
 	   	double bagelprices[]={3.00,4.00,5.00};
 	   	
-	   	cout<<"1. "<<bageltype[0]<<"--"<<bagelprices[0]<<"dollers"<<endl;
-	   	cout<<"2. "<<bageltype[0]<<"--"<<bagelprices[1]<<"dollers"<<endl;
-	   	cout<<"3. "<<bageltype[0]<<"--"<<bagelprices[2]<<"dollers"<<endl;
+	   	cout<<"1. "<<bageltype[0]<<"--"<<bagelprices[0]<<" dollers "<<endl;
+	   	cout<<"2. "<<bageltype[0]<<"--"<<bagelprices[1]<<" dollers "<<endl;
+	   	cout<<"3. "<<bageltype[0]<<"--"<<bagelprices[2]<<" dollers "<<endl;
 	   	cout<<"your choice: "<<endl;
 	   	int q3;
 	   	cin>>q3;
+	   	
+	   	if(q3==1){
+	   		cout<<"you have ordered a "<<bageltype[0]<<endl;
+	   		cout<<"the total of your order is "<<bagelprices[0]<<" dollars "<<endl;
+	   		cout<<"you have "<<balance-bagelprices[0]<<"dollors left on your balance"<<endl;
+		   }else if(q3==2){
+		   	cout<<"you have ordered a "<<bageltype[1]<<endl;
+		    cout<<"the total of your order is "<<bagelprices[1]<<" dollars "<<endl;
+	   		cout<<"you have "<<balance-bagelprices[1]<<"dollors left on your balance"<<endl;
+		   }else if(q3==3){
+		   	cout<<"you have ordered a "<<bageltype[2]<<endl;
+		    cout<<"the total of your order is "<<bagelprices[2]<<" dollars "<<endl;
+	   		cout<<"you have "<<balance-bagelprices[2]<<"dollors left on your balance"<<endl;
+		   }
+	   	
 	   }
 	   else{
 	   	cout<<"Alright have a good day"<<endl;
